0eebef5b8e840313e2bd4ae3a85c93ffc89f591c
Igor Manoel <131622164+igormanoels@users.noreply.github.com>
2024-03-08 21:51:13 -0300

    Atividades de LP em Python

---
+++ Teste/executacmd.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def abrir_prompt_de_comando():
+    try:
+        subprocess.Popen(['cmd'])
+    except Exception as e:
+        print("Erro ao abrir o prompt de comando:", e)
+
+# Chamando a função para abrir o prompt de comando
+abrir_prompt_de_comando()
