fb4aa4b5a9272b888fd6e23418a9c44056b89ef3
igormanoels <s.igormanoel@gmail.com>
2024-09-16 00:16:03 -0300

    Correção do exercício 32.

--- Fatec Zona Leste - Disciplina de Linguagem de Programação/Lote 1.1/Exercicio_32.py
+++ Fatec Zona Leste - Disciplina de Linguagem de Programação/Lote 1.1/Exercicio_32.py
@@ -6,9 +6,10 @@ os.system('cls')
 fatorial = int(input("Informe um número para saber seu fatorial: "))
 
 if fatorial == 1 or fatorial == 0:
-    print(f"O fatorial é {fatorial}")
+    print("O fatorial é 1")
 elif fatorial > 1:
+    res = 1
     while fatorial > 1:
-        res =+ (fatorial * (fatorial - 1))
+        res += (res * (fatorial - 1))
         fatorial-=1
     print(f"O fatorial é {res}")
