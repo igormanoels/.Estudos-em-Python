2fa50fbf5e7f3acfcdfba496d097dab112417394
igormanoels <s.igormanoel@gmail.com>
2025-04-03 21:54:19 -0300

    Continuação do curso da Alura de Modelagem de Dados com Python, segunda lista de exercícios de Python.

--- Alura/01 - Primeiros Passos/Aula 02 - Manipulando Dados no Python/06 - desafio_01.py
+++ Alura/01 - Primeiros Passos/Aula 02 - Manipulando Dados no Python/06 - desafio_01.py
@@ -1,7 +1,17 @@
 # Crie um programa que solicite à pessoa usuária digitar seu nome, e imprima “Olá, [nome]!”.
+nome = input('Digite seu nome: ')
+print(nome)
 
 
 # Crie um programa que solicite à pessoa usuária digitar seu nome e idade, e imprima “Olá, [nome], você tem [idade] anos.”.
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+print(f'Olá, {nome}, você tem {idade} anos.')
 
 
 # Crie um programa que solicite à pessoa usuária digitar seu nome, idade e altura em metros, e imprima “Olá, [nome], você tem [idade] anos e mede [altura] metros!”.
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+altura = float(input('Informe sua altura: '))
+print(f'Olá, {nome}, você tem {idade} anos e mede {altura} metros!')
+
--- Alura/01 - Primeiros Passos/Aula 02 - Manipulando Dados no Python/08 - desafio_03.py
+++ Alura/01 - Primeiros Passos/Aula 02 - Manipulando Dados no Python/08 - desafio_03.py
@@ -1,29 +1,49 @@
 # Crie uma variável chamada “frase” e atribua a ela uma string de sua escolha. Em seguida, imprima a frase na tela.
+frase = "Uma frase a minha escolha kkkk"
+print(frase)
 
 
 # Crie um código que solicite uma frase e depois imprima a frase na tela.
+frase = input('Escreve uma frase: ')
+print(frase)
 
 
 # Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase digitada mas com todas as letras maiúsculas.
+frase = input('Escreve uma frase: ').upper()
+print(frase)
 
 
 # Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase digitada mas com todas as letras minúsculas.
+frase = input('Escreve uma frase: ').lower()
+print(frase)
 
 
 # Crie uma variável chamada “frase” e atribua a ela uma string de sua escolha. Em seguida, imprima a frase sem espaços em branco no início e no fim.
+frase = " Uma frase a minha escolha kkkk "
+print(frase.strip())
 
 
 # Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase sem espaços em branco no início e no fim.
+frase = input('Escreve uma frase: ').strip()
+print(frase)
 
 
 # Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase sem espaços em branco no início e no fim e em letras minúsculas.
+frase = input('Escreve uma frase: ').lower().strip()
+print(frase)
 
 
 # Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase com todas as vogais “e” trocadas pela letra “f”.
+frase = input('Escreve uma frase: ')
+print(frase.replace('e', 'f'))
 
 
 # Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase com todas as vogais “a” trocadas pela caractere “@”.
+frase = input('Escreve uma frase: ')
+print(frase.replace('a', '@'))
 
 
 # Crie um código que solicite uma frase à pessoa usuária e imprima a mesma frase com todas as consoantes “s” trocadas pelo caractere “$”.
+frase = input('Escreve uma frase: ')
+print(frase.replace('s', '$'))
 
