e16f56d4eb3cfcac798139340c33eb5211545ac4
igormanoels <s.igormanoel@gmail.com>
2025-05-11 12:24:08 -0300

    Continuação do curso da alura - aprendendo a fazer etl, exercício para módulo da Pandas.

--- Alura/G8 - ONE - TECH FOUNDATION - Especialização Data Science/02 - Aprendendo a fazer ETL/02 - Pandas - Manipulação e análise de dados/Desafios/01 - Análise sobre alunos.csv/main.py
+++ Alura/G8 - ONE - TECH FOUNDATION - Especialização Data Science/02 - Aprendendo a fazer ETL/02 - Pandas - Manipulação e análise de dados/Desafios/01 - Análise sobre alunos.csv/main.py
@@ -1,9 +1,16 @@
+import pandas as pd
+
 # 1) Importe o arquivo alunos.csv e armazene seu conteúdo em um DataFrame Pandas.
+arquivo = 'Alura/G8 - ONE - TECH FOUNDATION - Especialização Data Science/02 - Aprendendo a fazer ETL/02 - Pandas - Manipulação e análise de dados/Desafios/01 - Análise sobre alunos.csv/alunos.csv'
+dados = pd.read_csv(arquivo,sep=',')
 
 # 2) Visualize as primeiras 7 linhas do DataFrame e as 5 últimas.
+print('\n', dados.head(7))
 
 # 3) Confira a quantidade de linhas e colunas desse DataFrame.
+print('\n', dados.shape)
 
 # 4) Explore as colunas do DataFrame e analise os tipos dos dados presentes em cada coluna.
+print('\n', dados[['Idade', 'Aprovado']])
 
 # Extra: Calcule algumas estatísticas descritivas básicas dos dados do DataFrame (média, desvio padrão, etc). Dica: pesquise pelo método describe.
